3f9879125372bfd959be8fc071be1a53de41f387
Siddhartha Sarkar <mail.04.siddhartha@gmail.com>
2024-02-23 02:07:57 +0530

    delete functionality

--- redux-toolkit/redux-toolkit/src/components/TodoList/ToDoList.jsx
+++ redux-toolkit/redux-toolkit/src/components/TodoList/ToDoList.jsx
@@ -1,17 +1,29 @@
 import { useState } from "react";
-import { useDispatch } from 'react-redux';
-import { addTodo } from "../../store/slice";
+import { useDispatch, useSelector } from "react-redux";
+import { addTodo, deleteTodo } from "../../store/slice";
+import Button from "@mui/material/Button";
+import CloudUploadIcon from "@mui/material/Icon";
 
 const ToDoList = () => {
   const [currentTodo, setCurrentTodo] = useState("");
   const dispatch = useDispatch();
 
+  const { todoList } = useSelector((state) => state.todo);
+
+  console.log(todoList);
+
   // console.log(currentTodo);
 
   const handleAddTodo = () => {
     dispatch(addTodo(currentTodo));
-  }
-  
+    setCurrentTodo("");
+  };
+
+  const handleDeleteTodo = (id) => {
+    dispatch(deleteTodo(id));
+  };
+
+  const handleUpdateTodo = (currentTodo) => {};
 
   return (
     <div>
@@ -22,7 +34,41 @@ const ToDoList = () => {
         name="todo"
         placeholder="Enter your todo"
       />
-      <button disabled={currentTodo === ''} onClick={handleAddTodo}>Add Todo</button>
+      <Button
+        variant="contained"
+        color="success"
+        disabled={currentTodo === ""}
+        onClick={handleAddTodo}
+      >
+        Add Todo
+      </Button>
+
+      <div>
+        <ul>
+          {todoList && todoList.length > 0
+            ? todoList.map((item) => (
+                <div>
+                  <li key={item.id}>
+                    <p> {item.title}</p>
+                    <Button
+                      variant="outlined"
+                      color="error"
+                      onClick={() => handleDeleteTodo(item.id)}
+                    >
+                      Delete
+                    </Button>
+                    <Button
+                      variant="outlined"
+                      onClick={() => handleUpdateTodo(item.id)}
+                    >
+                      Update
+                    </Button>
+                  </li>
+                </div>
+              ))
+            : null}
+        </ul>
+      </div>
     </div>
   );
 };
--- redux-toolkit/redux-toolkit/src/store/slice/index.js
+++ redux-toolkit/redux-toolkit/src/store/slice/index.js
@@ -12,19 +12,28 @@ const todoReducer = createSlice({
   reducers: {
     //reducers will combine all the actions that you need
     addTodo(state, action) {
-      console.log(action);
+      //   console.log(action);
 
       const newlyCreatedTodo = {
-        id: new Date(),
+        id: state.todoList.length === 0 ? 1 : state.todoList.length + 1,
         title: action.payload,
       };
 
       state.todoList.push(newlyCreatedTodo);
       return state;
     },
+    deleteTodo(state, action) {
+      console.log(action);
+
+      state.todoList = state.todoList.filter(
+        (todoItem) => todoItem.id !== action.payload
+      );
+
+      return state;
+    },
   },
 });
 
-export const { addTodo } = todoReducer.actions;
+export const { addTodo, deleteTodo } = todoReducer.actions;
 
 export default todoReducer.reducer;
